8eec07e110bc435436c55a3cf17fc6733660c17a
abelmuro <abelmuro93@gmail.com>
2025-09-06 11:46:37 -0700

    commit

--- src/Routes/POST/AI_Move.js
+++ src/Routes/POST/AI_Move.js
@@ -6,34 +6,32 @@ const ConvertToFen = require('./Utility/ConvertToFen');
 const ConvertAlgebraicToCheckers = require('./Utility/ConvertAlgebraicToCheckers');
 
 /*
-    this is where i left off, i need to continue to find a way to use stockfish for a checkers match
+    this is where i left off, i need to use another checkers engine to calculate the best move
 
 */
 
 router.post('/ai_move', (req, res) => {
     const {board} = req.body;
     const FEN = ConvertToFen(board);
-    console.log(FEN)
  
     try{
         const enginePath = path.resolve(__dirname, './../../ChildProcess/CheckersEngine/stockfishWindows.exe');
         const engine = spawn(enginePath);
 
         engine.stdin.write('uci\n');
-        engine.stdin.write('setoption name Skill Level value 5\n');     //0 easiest, 20 strongest
         engine.stdin.write('isready\n');    
 
         engine.stdout.on('data', (data) => {
             const output = data.toString();
             console.log(output)
             if(output.includes('readyok')){
-                engine.stdin.write(`position ${FEN}\n`);
+                engine.stdin.write(`position B:W12,13,14,15:B1,2,3,4\n`);
                 engine.stdin.write('go movetime 1000\n');
             }  
             else if(output.includes('bestmove')){
                 const bestmoveOutput = output.slice(output.indexOf('bestmove'), output.length);
                 const bestmove = bestmoveOutput.split(' ')[1];
-                const checkersSquare = `${ConvertAlgebraicToCheckers(bestmove[0], Number(bestmove[1]))} ${ConvertAlgebraicToCheckers(bestmove[2], Number(bestmove[3]))}`
+                const checkersSquare = ConvertAlgebraicToCheckers(bestmove);
                 res.status(200).send(checkersSquare);                    
             }
         });
--- src/Routes/POST/Utility/ConvertAlgebraicToCheckers.js
+++ src/Routes/POST/Utility/ConvertAlgebraicToCheckers.js
@@ -1,8 +1,18 @@
-function ConvertAlgebraicToCheckers(file, rank) {
-  const darkSquareRanks = [1, 2, 3, 4, 5, 6, 7, 8];             
-  const darkSquareFiles = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];   //columns
-  const index = darkSquareRanks.indexOf(rank) * 4 + darkSquareFiles.indexOf(file);
-  return index + 1; // checkers squares are 1-indexed
+function ConvertAlgebraicToCheckers(move) {
+  const chessToCheckers = {
+      b8: 1,  d8: 2,  f8: 3,  h8: 4,
+      a7: 5,  c7: 6,  e7: 7,  g7: 8,
+      b6: 9,  d6: 10, f6: 11, h6: 12,
+      a5: 13, c5: 14, e5: 15, g5: 16,
+      b4: 17, d4: 18, f4: 19, h4: 20,
+      a3: 21, c3: 22, e3: 23, g3: 24,
+      b2: 25, d2: 26, f2: 27, h2: 28,
+      a1: 29, c1: 30, e1: 31, g1: 32
+  };
+  const from = move.slice(0, 2);
+  const to = move.slice(2, move.length);
+
+  return `${chessToCheckers[from]} ${chessToCheckers[to]}`;                                            
 }
 
 module.exports = ConvertAlgebraicToCheckers;
