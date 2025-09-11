3a13f66ab6e165015073fd3ab48337eb8e4d30f1
AbelMuro <abelmuro93@gmail.com>
2025-09-11 15:50:54 -0700

    commit

--- src/Routes/POST/AI_Move.js
+++ src/Routes/POST/AI_Move.js
@@ -3,10 +3,18 @@ const findBestMove = require('./Utility/CalculateBestMove');
 const router = express.Router();
 
 router.post('/ai_move', (req, res) => {
-    const {board, color} = req.body;
+    const {board, color, difficulty} = req.body;
+    let depth;
+
+    if(difficulty === 'easy')
+        depth = 3;
+    else if(difficulty === 'medium')
+        depth = 6;
+    else
+        depth = 10;
 
     try{
-        const bestMove = findBestMove(board, 5, color);
+        const bestMove = findBestMove(board, depth, color);
         res.status(200).json(bestMove);
     }
     catch(error){
