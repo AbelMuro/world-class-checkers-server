ad46c13d2f2194094f0cb6c21555bcbe19d92475
AbelMuro <abelmuro93@gmail.com>
2025-09-12 14:49:54 -0700

    commit

--- src/Routes/POST/Utility/CalculateBestMove.js
+++ src/Routes/POST/Utility/CalculateBestMove.js
@@ -16,56 +16,65 @@ function getLegalMoves(board, playerColor) {
                 rightCorner: {row: row + direction, col: col + 1},
             } 
 
+            /* 
             const kingMoveSquares = {
                 topLeftCorner: {row: row + 1, col: col - 1},
                 topRightCorner: {row: row + 1, col: col + 1},
                 bottomLeftCorner: {row: row - 1, col: col - 1},
                 bottomRightCorner: {row: row - 1, col: col + 1}
-            }
+            }       
+            const kingJumpSquares = {
+                topLeftCorner: {row: row + 2, col: col - 2, capture: kingMoveSquares.topLeftCorner},
+                topRightCorner: {row: row + 2, col: col + 2, capture: kingMoveSquares.topRightCorner},
+                bottomLeftCorner: {row: row - 2, col: col - 2, capture: kingMoveSquares.bottomLeftCorner},
+                bottomRightCorner: {row: row - 2, col: col + 2, capture: kingMoveSquares.bottomRightCorner}
+            }                
+            
+            */
+
 
             const jumpSquares = {
                 leftCorner: {row: row + direction + direction, col: col - 2, capture: moveSquares.leftCorner},
                 rightCorner: {row: row + direction + direction, col: col + 2, capture: moveSquares.rightCorner}
             }
 
-            const kingJumpSquares = {
-                topLeftCorner: {row: row + 2, col: col - 2, capture: kingMoveSquares.topLeftCorner},
-                topRightCorner: {row: row + 2, col: col + 2, capture: kingMoveSquares.topRightCorner},
-                bottomLeftCorner: {row: row - 2, col: col - 2, capture: kingMoveSquares.bottomLeftCorner},
-                bottomRightCorner: {row: row - 2, col: col + 2, capture: kingMoveSquares.bottomRightCorner}
-            }
 
-            if(cell.includes('queen')){
-                for (const square of Object.values(kingMoveSquares)) {
-                    const r = square.row;
-                    const c = square.col;
 
-                    if(board[r][c] === ''){
-                        moves.push({
-                            from: {row, col},
-                            to: {row: r, col: c},
-                            piece: cell
-                        });                    
-                    }                     
-                }
-                for(const square of Object.values(kingJumpSquares)){
-                    const r = square.row;
-                    const c = square.col;
-                    const capture = square.capture;
+            /* 
+                if(cell.includes('queen')){
+                    for (const square of Object.values(kingMoveSquares)) {
+                        const r = square.row;
+                        const c = square.col;
 
-                    if (board[r][c] === ''){
-                        if(board[capture.row]?.[capture.col]?.includes(opposingPlayer)){
+                        if(board[r][c] === ''){
                             moves.push({
-                                capture: {...capture, pieceId: board[capture.row][capture.col]},
                                 from: {row, col},
                                 to: {row: r, col: c},
                                 piece: cell
-                            })                        
-                        }
+                            });                    
+                        }                     
                     }
-                } 
-            }
-            else{
+                    for(const square of Object.values(kingJumpSquares)){
+                        const r = square.row;
+                        const c = square.col;
+                        const capture = square.capture;
+
+                        if (board[r][c] === ''){
+                            if(board[capture.row]?.[capture.col]?.includes(opposingPlayer)){
+                                moves.push({
+                                    capture: {...capture, pieceId: board[capture.row][capture.col]},
+                                    from: {row, col},
+                                    to: {row: r, col: c},
+                                    piece: cell
+                                })                        
+                            }
+                        }
+                    } 
+                }            
+            
+            
+            */
+
                 for (const square of Object.values(moveSquares)) {
                     const r = square.row;
                     const c = square.col;
@@ -94,8 +103,7 @@ function getLegalMoves(board, playerColor) {
                             })                        
                         }
                     }
-                }                
-            }    
+                }                  
       }
     }
   }
